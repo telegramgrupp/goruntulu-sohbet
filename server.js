c8e1cadae9d2a86b81bda917a4436e8707e6a5a9
telegramgrupp <muratctlll@gmail.com>
2025-05-05 23:29:36 +0300

    server.js eşleştirme düzeltmesi eklendi

--- server.js
+++ server.js
@@ -780,3 +780,7 @@ io.on('connection', (socket) => {
     console.log(`Sunucu ${PORT} portunda çalışıyor`);
 });
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda çalışıyor`);
+});
